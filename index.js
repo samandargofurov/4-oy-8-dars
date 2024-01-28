824d829503195d82381ef81073c451be5454406f
samandargofurov <samandargofurov236@gmail.com>
2024-01-28 08:39:16 +0500

    8 lesson

---
+++ index.js
@@ -0,0 +1,58 @@
+//  1. Kubning yon uzunligi berilgan hajmni hisoblaydigan funksiyani yozing.
+
+function kubningHajmi(arg) {
+    return arg ** 3;
+}
+    
+let res = kubningHajmi(3);
+console.log(res);
+
+//  2. Satrda ma'lum bir belgi bor yoki yo'qligini tekshiradigan funktsiyani aniqlang.
+
+
+
+//  3. Massivdagi birinchi N natural sonni qaytaruvchi funksiya yarating.
+
+
+
+//  4. Sondagi raqamlar yig‘indisini hisoblaydigan funksiya ishlab chiqing.
+
+
+
+//  5. Yilning kabisa yili ekanligini tekshiradigan funksiyani aniqlang.
+
+
+
+//  6. Belgilangan ikkita qiymat orasida tasodifiy son hosil qiluvchi funksiya yarating.
+
+
+
+//  7. To‘g‘ri to‘rtburchakning kengligi va balandligini hisobga olgan holda uning maydonini hisoblaydigan funksiya ishlab chiqing.
+
+
+
+//  8. Farengeytni Selsiyga aylantiruvchi funksiyani aniqlang.
+
+//  9. Uchburchakning teng yonli, teng yonli yoki masshtabli ekanligini tekshiradigan funksiya yarating.
+
+//  10. Ikki sonning minimalini topuvchi funksiya ishlab chiqing
+
+//  11. Doira radiusi berilgan doiraning maydonini hisoblaydigan funksiyani aniqlang.
+
+//  12. Satrning palindrom ekanligini tekshiradigan funksiya yarating (tinish belgilariga e’tibor bermay).
+
+//  13. Silindrning radiusi va balandligini hisobga olgan holda uning hajmini hisoblaydigan funksiyani ishlab chiqing.
+
+//  14. To‘g‘ri to‘rtburchak ichida nuqta yotishini tekshiradigan funksiyani aniqlang.
+
+//  15. Alifboning tasodifiy harfini hosil qiluvchi funksiya yarating.
+
+//  16. Uchburchakning tomonlari uzunliklari berilgan perimetrini hisoblaydigan funksiyani ishlab chiqing.
+
+//  17. Sonning toq mukammal kvadrat ekanligini tekshiradigan funksiyani aniqlang.
+
+//  18. Belgilangan ikkita qiymat orasida tasodifiy juft son hosil qiluvchi funksiya yarating.
+
+//  19. Konusning radiusi va balandligini hisobga olgan holda uning hajmini hisoblaydigan funktsiyani tuzing.
+
+//  20. Satrning haqiqiy telefon raqami ekanligini tekshiradigan funksiyani aniqlang (asosiy formatni tekshirish).
